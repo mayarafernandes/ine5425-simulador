dbd4fa955ecd90762c0671f6a7f01107fabb0372
Mayara <mayarafernands@outlook.com>
2017-05-24 10:03:27 -0300

    Adding Solution

---
+++ ModelSim/Principal.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ModelSim
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.buttonCreateChart = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreateChart
+            // 
+            this.buttonCreateChart.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreateChart.Name = "buttonCreateChart";
+            this.buttonCreateChart.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreateChart.TabIndex = 0;
+            this.buttonCreateChart.Text = "Create Chart";
+            this.buttonCreateChart.UseVisualStyleBackColor = true;
+            this.buttonCreateChart.Click += new System.EventHandler(this.buttonCreateChart_Click);
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 66);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(368, 300);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 378);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.buttonCreateChart);
+            this.Name = "Principal";
+            this.Text = "Chart";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreateChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ ModelSim/Principal.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ModelSim
+{
+    public partial class Principal : Form
+    {
+        public int ChartType { get; set; }
+
+        public Principal()
+        {
+            InitializeComponent();
+            this.ChartType = 3;
+            this.DrawChart();
+        }
+
+        private void buttonCreateChart_Click(object sender, EventArgs e)
+        {
+            this.DrawChart();
+        }
+
+        private void DrawChart()
+        {
+            this.chart.Titles.Clear();
+            this.chart.Series.Clear();
+
+            if (this.ChartType == 1)
+            {
+                // Data arrays
+                string[] seriesArray = { "Cat", "Dog", "Bird", "Monkey" };
+                int[] pointsArray = { 2, 1, 7, 5 };
+
+                // Set palette
+                this.chart.Palette = ChartColorPalette.EarthTones;
+
+                // Set title            
+                this.chart.Titles.Add("Animals");
+
+                // Add series.
+                for (int i = 0; i < seriesArray.Length; i++)
+                {
+                    Series series = this.chart.Series.Add(seriesArray[i]);
+                    series.Points.Add(pointsArray[i]);
+                }
+                this.ChartType = 2;
+            }
+            else if (this.ChartType == 2)
+            {
+                this.chart.Titles.Add("Total Income");
+
+                Series series = this.chart.Series.Add("Total Income");
+                series.ChartType = SeriesChartType.Spline;
+                series.Points.AddXY("September", 100);
+                series.Points.AddXY("Obtober", 300);
+                series.Points.AddXY("November", 800);
+                series.Points.AddXY("December", 200);
+                series.Points.AddXY("January", 600);
+                series.Points.AddXY("February", 400);
+
+                this.ChartType = 3;
+            }
+            else if (this.ChartType == 3)
+            {
+                this.chart.Titles.Add("Total Income");               
+
+                Series series = this.chart.Series.Add("Total Income");
+
+                series.ChartType = SeriesChartType.Line;
+                for (int i = -5; i < 6; i++)
+                    series.Points.AddXY(i, i - 2);
+
+                series = this.chart.Series.Add("Total Income 2");
+
+                series.MarkerStyle = MarkerStyle.Diamond;
+                series.MarkerColor = Color.Blue;
+                
+                series.ChartType = SeriesChartType.Line;
+                for (int i = -5; i < 6; i++)
+                    series.Points.AddXY(i, i - 4);
+            }
+
+        }
+    }
+}
